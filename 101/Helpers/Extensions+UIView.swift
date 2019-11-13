5026147a45fb0d33ccd93a7e25b06449c44332d1
Abdullah Alsharif <ammsh92@gmail.com>
2019-11-13 18:33:44 +0300

    SECOND LESSON

---
+++ 101/Helpers/Extensions+UIView.swift
@@ -0,0 +1,362 @@
+//
+//  Extensions+UIView.swift
+//  SlideOutMenuInProgress
+//
+//  Created by Brian Voong on 9/30/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+extension UITapGestureRecognizer {
+    
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: label.attributedText!)
+        
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+        
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y);
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x,y: locationOfTouchInLabel.y - textContainerOffset.y);
+        
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+    
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+    
+}
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+struct AnchoredConstraints {
+    var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
+extension UIImage {
+    func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager.default()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
+    }
+}
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
+
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
+extension UINavigationBar {
+    func setGradientBackground(colors: [Any]) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.locations = [0.0 , 0.5, 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        var updatedFrame = self.bounds
+        updatedFrame.size.height += self.frame.origin.y
+        gradient.frame = updatedFrame
+        gradient.colors = colors;
+        self.setBackgroundImage(self.image(fromLayer: gradient), for: .default)
+    }
+    
+    func image(fromLayer layer: CALayer) -> UIImage {
+        UIGraphicsBeginImageContext(layer.frame.size)
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let outputImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return outputImage!
+    }
+}
+extension CAGradientLayer {
+    
+    enum Point {
+        case topRight, topLeft
+        case bottomRight, bottomLeft
+        case custion(point: CGPoint)
+        
+        var point: CGPoint {
+            switch self {
+            case .topRight: return CGPoint(x: 1, y: 0)
+            case .topLeft: return CGPoint(x: 0, y: 0)
+            case .bottomRight: return CGPoint(x: 1, y: 1)
+            case .bottomLeft: return CGPoint(x: 0, y: 1)
+            case .custion(let point): return point
+            }
+        }
+    }
+    
+    convenience init(frame: CGRect, colors: [UIColor], startPoint: CGPoint, endPoint: CGPoint) {
+        self.init()
+        self.frame = frame
+        self.colors = colors.map { $0.cgColor }
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+    }
+    
+    convenience init(frame: CGRect, colors: [UIColor], startPoint: Point, endPoint: Point) {
+        self.init(frame: frame, colors: colors, startPoint: startPoint.point, endPoint: endPoint.point)
+    }
+    
+    func createGradientImage() -> UIImage? {
+        defer { UIGraphicsEndImageContext() }
+        UIGraphicsBeginImageContext(bounds.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
+
+extension UINavigationBar {
+    func setGradientBackground(colors: [UIColor], startPoint: CAGradientLayer.Point = .topLeft, endPoint: CAGradientLayer.Point = .bottomLeft) {
+        var updatedFrame = bounds
+        updatedFrame.size.height += self.frame.origin.y
+        let gradientLayer = CAGradientLayer(frame: updatedFrame, colors: colors, startPoint: startPoint, endPoint: endPoint)
+        setBackgroundImage(gradientLayer.createGradientImage(), for: UIBarMetrics.default)
+    }
+}
+
+extension Collection {
+    func insertionIndex(of element: Self.Iterator.Element,
+                        using areInIncreasingOrder: (Self.Iterator.Element, Self.Iterator.Element) -> Bool) -> Index {
+        return index(where: { !areInIncreasingOrder($0, element) }) ?? endIndex
+    }
+}
+
+
+extension Int {
+    var roundedWithAbbreviations: String {
+        let number = Double(self)
+        let thousand = number / 1000
+        let million = number / 1000000
+        if million >= 1.0 {
+            return "\(round(million*10)/10)M"
+        }
+        else if thousand >= 1.0 {
+            return "\(round(thousand*10)/10)K"
+        }
+        else {
+            return "\(self)"
+        }
+    }
+}
+
+extension UIButton {
+    func applyGradient(colors: [CGColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colors
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
+extension NSLayoutAnchor {
+    @objc func constraintEqualToAnchor(anchor: NSLayoutAnchor!, constant:CGFloat, identifier:String) -> NSLayoutConstraint! {
+        let constraint = self.constraint(equalTo: anchor, constant:constant)
+        constraint.identifier = identifier
+        return constraint
+    }
+}
+extension UIView {
+    func constraint(withIdentifier:String) -> NSLayoutConstraint? {
+        return self.constraints.filter{ $0.identifier == withIdentifier }.first
+    }
+}
+extension UIView {
+
+    
+    
+    @discardableResult
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top {
+            anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top)
+        }
+        
+        if let leading = leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+        
+        if let bottom = bottom {
+            anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
+        }
+        
+        if let trailing = trailing {
+            anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
+        }
+        
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{ $0?.isActive = true }
+        
+        return anchoredConstraints
+    }
+    
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor {
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right).isActive = true
+        }
+    }
+    
+    func centerInSuperview(size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    func constrainHeight(_ constant: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: constant).isActive = true
+    }
+    
+    func constrainWidth(_ constant: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: constant).isActive = true
+    }
+    
+    func centerViewX(view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func centerViewY(view: UIView, constant: CGFloat?) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant ?? 0).isActive = true
+    }
+    
+    @discardableResult
+    func stack(_ axis: NSLayoutConstraint.Axis = .vertical, views: UIView..., spacing: CGFloat = 0) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: views)
+        stackView.axis = axis
+        stackView.spacing = spacing
+        addSubview(stackView)
+        stackView.fillSuperview()
+        return stackView
+    }
+    
+    func setupShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+    }
+    
+    convenience init(backgroundColor: UIColor = .clear) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+    
+}
+
+extension UIStackView {
+    
+    @discardableResult
+    func withMargins(_ margins: UIEdgeInsets) -> UIStackView {
+        layoutMargins = margins
+        isLayoutMarginsRelativeArrangement = true
+        return self
+    }
+    
+    @discardableResult
+    func padLeft(_ left: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.left = left
+        return self
+    }
+    
+    @discardableResult
+    func padTop(_ top: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.top = top
+        return self
+    }
+}
+import UIKit
+import MapKit
+
+class MyAnnotation: NSObject,MKAnnotation {
+    
+    var title : String?
+    var subTit : String?
+    var coordinate : CLLocationCoordinate2D
+    
+    init(title:String,coordinate : CLLocationCoordinate2D,subtitle:String){
+        
+        self.title = title;
+        self.coordinate = coordinate;
+        self.subTit = subtitle;
+        
+    }
+    
+}
---
+++ 101/Logoin/LoginView.swift
@@ -0,0 +1,9 @@
+//
+//  LoginView.swift
+//  101
+//
+//  Created by Alsharif Abdullah M on 11/11/19.
+//  Copyright © 2019 Alsharif Abdullah M. All rights reserved.
+//
+
+import Foundation
